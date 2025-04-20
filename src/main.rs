787d4783f47ab2c32b372fbe60fef9ee9ba354b1
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-04-20 12:20:52 +0700

    enhance: added search and case insensitive search. with simple TDD implementation

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {err}");
+        eprintln!("Problem parsing argument: {err}");
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     
     
     if let Err(e) = run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
 
     }
