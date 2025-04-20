209c6561607bddf29b362df02f1c5aac6d95e7c3
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-04-20 11:58:28 +0700

    enhance: moved functionalities to lib for seperate of concerns

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::env;
-use std::fs;
 use std::process;
+use std::env;
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,27 +15,12 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    
+    
+    if let Err(e) = run(config){
+        println!("Application error: {e}");
+        process::exit(1)
 
-    println!("With text:\n{contents}");
-
-}
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3  {
-            return  Err("not enough arguments!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
+
 }
